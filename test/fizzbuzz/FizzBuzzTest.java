8158654943c16679b94eb2ffe312cda4efc21ba6
Dev-Zaius <m.paquet32@laposte.net>
2023-04-21 14:01:10 +0200

    Création du repositery Git pour le TP

---
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+	@Test
+	void test() {
+		fail("not yet implemented");
+	}
+
+}
